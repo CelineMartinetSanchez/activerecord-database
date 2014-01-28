cf1dbb14cef8899851e504758d31578dfc49b06f
Denver <denver@denver-Computer.(none)>
2014-01-28 14:52:14 +0100

    Our amazing cookbook'

--- 05.CookBook/cookbook.rb
+++ 05.CookBook/cookbook.rb
@@ -5,16 +5,36 @@ require './models/recipe'
 
 puts "Salut Robuchon, what do you want to do today?"
 puts "1. create a recipe"
-puts "2. delete all recipes"
-puts "3. read your recipes"
+puts "2. delete a recipe"
+puts "3. delete all recipes"
+puts "4. read your recipes"
 
 choice = gets.chomp.to_i
 
 if choice == 1
-  
+	puts "Please enter a name."
+	name = gets.chomp
+	puts "Please enter a description."
+	description = gets.chomp
+	puts "What is the length? (on minutes)"
+	length = gets.chomp.to_i
+	puts "And the difficulty? (5 being the maximum)"
+	difficulty = gets.chomp.to_i
+  new_recipe = Recipe.create(name: name, description: description, length: length, difficulty: difficulty)
+  puts "Thanks! You added the following recipe: #{new_recipe.to_s}"
+
 elsif choice == 2
-  
+	puts "What recipe do you want to delete? Enter its id."
+	puts "--------------------------"
+	Recipe.all.each {|recipe| puts "#{recipe.name} : type #{recipe.id}"}
+	id = gets.chomp
+	Recipe.delete(id)
+
 elsif choice == 3
+	Recipe.delete_all()
+ 
+elsif choice == 4
+  puts Recipe.all
   
 else
   puts "i did not understand"
--- 05.CookBook/models/recipe.rb
+++ 05.CookBook/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ActiveRecord::Base
+
+	def to_s
+		"-- #{name} -- \nDescription: #{description} - Length: #{length} minutes - Difficulty: #{difficulty}."
+	end
 end
